d36b4447f22d9b32f576ed76c3a158b9cc6fadb9
sawaikatyou <mega_red@mac.com>
2024-07-25 22:36:11 +0900

    Feature/digital meter (#3)
    * add
    
    * digital meter

--- lib/src/bloc/meter_main_bloc.dart
+++ lib/src/bloc/meter_main_bloc.dart
@@ -27,20 +27,28 @@ class MeterMainState {
     this.igON,
     this.winkerLeftOn,
     this.winkerRightOn,
+    this.digitalMeterInformation,
   );
 
   final double speedKmh;
   final bool igON;
   final bool winkerLeftOn;
   final bool winkerRightOn;
+  final List<List<bool>> digitalMeterInformation;
 
-  MeterMainState copyWith(
-      {double? speedKmh, bool? igOn, bool? winkerLeftOn, bool? winkerRightOn}) {
+  MeterMainState copyWith({
+    double? speedKmh,
+    bool? igOn,
+    bool? winkerLeftOn,
+    bool? winkerRightOn,
+    List<List<bool>>? digitalMeterInformation,
+  }) {
     return MeterMainState(
       speedKmh ?? this.speedKmh,
       igOn ?? this.igON,
       winkerLeftOn ?? this.winkerLeftOn,
       winkerRightOn ?? this.winkerRightOn,
+      digitalMeterInformation ?? this.digitalMeterInformation,
     );
   }
 }
@@ -49,7 +57,7 @@ class MeterMainBloc extends Bloc<MeterEvent, MeterMainState> {
   final Location location = Location();
   StreamSubscription<LocationData>? _locationSubscription;
 
-  MeterMainBloc() : super(const MeterMainState(0.0, false, false, false)) {
+  MeterMainBloc() : super(const MeterMainState(0.0, false, false, false, [])) {
     on<_InitEvent>((event, emit) {
       // speed 監視
       _locationSubscription =
@@ -64,10 +72,30 @@ class MeterMainBloc extends Bloc<MeterEvent, MeterMainState> {
       });
     });
 
+    List<List<bool>> makeInformation({bool? newValue}) {
+      // TODO: ここにしかるべき処理を作る
+      // TODO: 現状は単に ig on / off で全部つけてるだけ
+      bool value = newValue ?? state.igON;
+      List<List<bool>> result = [];
+      print('[sasaki] makeInformation() ${state.igON}');
+
+      for (int i = 0; i < 4; i++) {
+        List<bool> parameter = <bool>[];
+        for (int j = 0; j < 7; j++) {
+          parameter.add(value);
+        }
+        result.add(parameter);
+      }
+
+      return result;
+    }
+
     on<_InnerSpeedUpdated>((event, emit) {
+      final information = makeInformation();
       emit(
         state.copyWith(
           speedKmh: event.nextState.speedKmh,
+          digitalMeterInformation: information,
         ),
       );
     });
@@ -89,7 +117,10 @@ class MeterMainBloc extends Bloc<MeterEvent, MeterMainState> {
     });
 
     on<IgChangeEvent>((event, emit) {
-      emit(state.copyWith(igOn: !state.igON));
+      emit(state.copyWith(
+        igOn: !state.igON,
+        digitalMeterInformation: makeInformation(newValue: !state.igON),
+      ));
     });
 
     add(_InitEvent());
---
+++ lib/src/ui/digital_speed_o_meter.dart
@@ -0,0 +1,266 @@
+import 'dart:math';
+
+import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:meter/src/bloc/meter_main_bloc.dart';
+
+@immutable
+class DigitalSpeedOMeterPaint extends StatelessWidget {
+  const DigitalSpeedOMeterPaint(
+      {super.key,
+      required this.width,
+      required this.height,
+      required this.color,
+      required this.index});
+
+  final double width;
+  final double height;
+  final Color color;
+  final int index;
+
+  @override
+  Widget build(BuildContext context) {
+    return BlocBuilder<MeterMainBloc, MeterMainState>(
+      builder: (context, state) {
+        late List<bool> inputValues;
+        if (state.digitalMeterInformation.isEmpty) {
+          inputValues = <bool>[];
+          for (int i = 0; i < 7; i++) {
+            inputValues.add(false);
+          }
+        } else {
+          inputValues = state.digitalMeterInformation[index];
+        }
+
+        return CustomPaint(
+          painter: DigitalMeterPainter(context, inputValues, color),
+          size: Size(width, height),
+        );
+      },
+    );
+  }
+}
+
+enum _GraphPaintSetting {
+  normal, // 通常設定 on,off 描画を使い分ける
+  translucent, // 描画しない
+  disable, // off描画
+  color, // on描画
+  rect, // Pathでなく rect で描画
+}
+
+/// デジタル８の字式表示ロジックの本体
+class DigitalMeterPainter extends CustomPainter {
+  DigitalMeterPainter(
+    this.context,
+    this.values,
+    this.color,
+  );
+
+  BuildContext context;
+  List<bool> values;
+  Color color;
+
+  // 内枠のPadding
+  static const kPadding = 5.0;
+
+  // 枠線の太さ
+  static const kBarWidth = 20.0;
+
+  // 各線ごとの固定設定、この値は values よりも優先させる
+  static const kEnabledSetting = <_GraphPaintSetting>[
+    _GraphPaintSetting.normal,
+    _GraphPaintSetting.normal,
+    _GraphPaintSetting.normal,
+    _GraphPaintSetting.normal,
+    _GraphPaintSetting.normal,
+    _GraphPaintSetting.normal,
+    _GraphPaintSetting.normal,
+  ];
+
+  static final sToneDownPaint = Paint()..color = Colors.black87;
+
+  /// 桁数の中で線を演算
+  @override
+  void paint(Canvas canvas, Size size) {
+    final colorPaint = Paint()..color = color;
+    final toneDownPaint = Paint()..color = Colors.black87;
+    final width = size.width;
+    final height = size.height;
+    final halfH = (height * 0.5);
+    const halfBar = (kBarWidth * 0.5);
+
+    //   00000
+    // 1       2
+    // 1       2
+    //   33333
+    // 4       5
+    // 4       5
+    //   66666
+    final rightBase = width - kBarWidth;
+    final drawRect = <Rect>[
+      Rect.fromLTWH(0, 0, width, kBarWidth),
+      Rect.fromLTWH(0, 0, kBarWidth, halfH),
+      Rect.fromLTWH(rightBase, 0, kBarWidth, halfH),
+      Rect.fromLTWH(0, halfH - halfBar, width, kBarWidth),
+      Rect.fromLTWH(0, halfH, kBarWidth, halfH),
+      Rect.fromLTWH(rightBase, halfH, kBarWidth, halfH),
+      Rect.fromLTWH(0, height - kBarWidth, width, kBarWidth),
+    ];
+
+    for (int i = 0; i < drawRect.length; i++) {
+      final rect = drawRect[i];
+      final l = rect.left;
+      final t = rect.top;
+      final w = rect.width;
+      final h = rect.height;
+
+      Path? path;
+      switch (i) {
+        case 0:
+          // (0-1) ------- (0-2)
+          //   ＼           ／
+          //   (0-4) --- (0-3)
+          path = Path()
+            ..moveTo(l + kPadding, t) // 0-1
+            ..lineTo(l + w - kPadding, t) // 0-2
+            ..lineTo(l + w - kBarWidth - kPadding, t + kBarWidth) // 0-3
+            ..lineTo(l + kBarWidth + kPadding, t + kBarWidth) // 0-4
+            ..close();
+          break;
+
+        case 1:
+          // (1-1) ＼
+          //   |     (1-2)
+          //   |       |
+          //   |       |
+          //   |     (1-3)
+          //   |   ／
+          // (1-4)
+          path = Path()
+            ..moveTo(l, t + kPadding) // 1-1
+            ..lineTo(l + kBarWidth, t + kBarWidth + kPadding) // 1-2
+            ..lineTo(l + kBarWidth, t + h - halfBar - kPadding) // 1-3
+            ..lineTo(l, t + h - kPadding) // 1-4
+            ..close();
+          break;
+
+        case 2:
+          //       (2-2)
+          //     ／  |
+          // (2-1)   |
+          //   |     |
+          // (2-4)   |
+          //     ＼  |
+          //       (2-3)
+          path = Path()
+            ..moveTo(l, t + kBarWidth + kPadding) // 2-1
+            ..lineTo(l + w, t + kPadding) // 2-2
+            ..lineTo(l + w, t + h - kPadding) // 2-3
+            ..lineTo(l, t + h - halfBar - kPadding) // 2-4
+            ..close();
+          break;
+
+        case 4:
+          // (4-1) ＼
+          //   |     (4-2)
+          //   |       |
+          //   |       |
+          //   |     (4-3)
+          //   |   ／
+          // (4-4)
+          path = Path()
+            ..moveTo(l, t + kPadding) // 4-1
+            ..lineTo(l + kBarWidth, t + kBarWidth - kPadding) // 4-2
+            ..lineTo(l + kBarWidth, t + h - kBarWidth - kPadding) // 4-3
+            ..lineTo(l, t + h - kPadding) // 4-4
+            ..close();
+          break;
+
+        case 5:
+          //       (5-2)
+          //     ／  |
+          // (５-1)  |
+          //   |     |
+          // (5-4)   |
+          //     ＼  |
+          //       (5-3)
+          path = Path()
+            ..moveTo(l, t + kBarWidth - kPadding) // 5-1
+            ..lineTo(l + w, t + kPadding) // 5-2
+            ..lineTo(l + w, t + h - kPadding) // 5-3
+            ..lineTo(l, t + h - kBarWidth - kPadding) // 5-4
+            ..close();
+          break;
+
+        case 3:
+          //      (3-2) --- (3-3)
+          //     ／             ＼
+          // (3-1)              (3-4)
+          //     ＼             ／
+          //      (3-6) --- (3-5)
+          path = Path()
+            ..moveTo(l + kPadding, t + halfBar) // 3-1
+            ..lineTo(l + kBarWidth + kPadding * 2, t) // 3-2
+            ..lineTo(l + w - kBarWidth - kPadding * 2, t) // 3-3
+            ..lineTo(l + w - kPadding, t + h * 0.5) // 3-4
+            ..lineTo(l + w - kBarWidth - kPadding * 2, t + h) // 3-3
+            ..lineTo(l + kBarWidth + kPadding * 2, t + h) // 3-2
+            ..close();
+          break;
+
+        case 6:
+          //   (6-1) --- (6-2)
+          //   ／           ＼
+          // (6-4) ------- (6-3)
+          path = Path()
+            ..moveTo(l + kPadding + kBarWidth, t) // 6-1
+            ..lineTo(l + w - kBarWidth - kPadding, t) // 6-2
+            ..lineTo(l + w - kPadding, t + h) // 6-3
+            ..lineTo(l + kPadding, t + h) // 6-4
+            ..close();
+          break;
+        default:
+          break;
+      }
+
+      switch (kEnabledSetting[i]) {
+        case _GraphPaintSetting.normal:
+          canvas.drawPath(path!, values[i] ? colorPaint : toneDownPaint);
+          break;
+        case _GraphPaintSetting.disable:
+          canvas.drawPath(path!, toneDownPaint);
+          break;
+        case _GraphPaintSetting.color:
+          canvas.drawPath(path!, colorPaint);
+          break;
+        case _GraphPaintSetting.rect:
+          canvas.drawRect(rect, Paint()..color = Colors.red);
+          break;
+        case _GraphPaintSetting.translucent:
+        default:
+          break;
+      }
+    }
+  }
+
+  @override
+  bool shouldRepaint(CustomPainter oldDelegate) => false;
+}
+
+class DigitalSpeedOMeterDot extends StatelessWidget {
+  const DigitalSpeedOMeterDot({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return BlocBuilder<MeterMainBloc, MeterMainState>(
+        buildWhen: (prev, cur) =>
+            prev.speedKmh != cur.speedKmh || prev.igON != cur.igON,
+        builder: (context, state) {
+          return Icon(
+            Icons.circle,
+            color: state.igON ? Colors.green : Colors.black87,
+          );
+        });
+  }
+}
